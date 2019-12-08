cff7d7674342db9104210d0443697cebe6154906
Cam <c4miloei@gmail.com>
2019-12-07 20:45:56 -0800

    Change context architecture to use reducer instead of multiple useState hooks

--- src/context.tsx
+++ src/context.tsx
@@ -1,58 +1,64 @@
-import React, { useState } from "react";
+import React, { useReducer, useContext } from "react";
 import { StepName, PlayerStat, Trophy, Log, Stats } from "./data";
 
-export interface ContextInterface {
-  step: StepName;
-  setStep: React.Dispatch<React.SetStateAction<StepName>>;
-  trophies: Array<Trophy>;
-  setTrophies: React.Dispatch<React.SetStateAction<Array<Trophy>>>;
-  logQueue: Array<Log>;
-  setLogQueue: React.Dispatch<React.SetStateAction<Array<Log>>>;
-  abandonedSouls: number;
-  setAbandonedSouls: React.Dispatch<React.SetStateAction<number>>;
-  unlockedAchievements: Array<string>;
-  setUnlockedAchievements: React.Dispatch<React.SetStateAction<Array<string>>>;
-  stats: Stats;
-  setStats: React.Dispatch<React.SetStateAction<Stats>>;
+interface Action {
+  type: string;
+  payload: any;
 }
 
-export const AppContext = React.createContext<ContextInterface>(
-  {} as ContextInterface
-);
+interface AppState {
+  step: StepName;
+  souls: number;
+  stats: Stats;
+  trophies: Array<Trophy>;
+  logs: Array<Log>;
+}
 
-const defaultStats: Stats = {
-  [PlayerStat.Attack]: 0,
-  [PlayerStat.Spirit]: 0,
-  [PlayerStat.Intelligence]: 0,
-  [PlayerStat.Determination]: 0
+interface Context {
+  state: AppState;
+  dispatch: (action: Action) => void;
+}
+
+const initialState: AppState = {
+  step: StepName.Introduction,
+  souls: 0,
+  stats: {
+    [PlayerStat.Attack]: 0,
+    [PlayerStat.Spirit]: 0,
+    [PlayerStat.Intelligence]: 0,
+    [PlayerStat.Determination]: 0
+  },
+  trophies: [],
+  logs: []
 };
 
-export default function AppProvider(props: any) {
-  const [step, setStep] = useState<StepName>(StepName.Introduction);
-  const [trophies, setTrophies] = useState<Array<Trophy>>([]);
-  const [logQueue, setLogQueue] = useState<Array<Log>>([]);
-  const [abandonedSouls, setAbandonedSouls] = useState(0);
-  const [unlockedAchievements, setUnlockedAchievements] = useState<
-    Array<string>
-  >([]);
-  const [stats, setStats] = useState<Stats>(defaultStats);
+const contextDefaultValue: Context = {
+  state: initialState,
+  dispatch: () => {}
+};
 
-  const value: ContextInterface = {
-    step,
-    setStep,
-    trophies,
-    setTrophies,
-    logQueue,
-    setLogQueue,
-    abandonedSouls,
-    setAbandonedSouls,
-    unlockedAchievements,
-    setUnlockedAchievements,
-    stats,
-    setStats
-  };
+const AppContext = React.createContext<Context>(contextDefaultValue);
+
+function globalReducer(state: AppState, action: Action) {
+  switch (action.type) {
+    case "setSouls":
+      const amount: number = action.payload;
+      return { ...state, souls: state.souls + amount };
+    default:
+      throw new Error();
+  }
+}
+
+export function AppProvider(props: any) {
+  const [state, dispatch] = useReducer(globalReducer, initialState);
 
   return (
-    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+    <AppContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </AppContext.Provider>
   );
+}
+
+export function useAppContext(): Context {
+  return useContext(AppContext);
 }
